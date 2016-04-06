83cd236843191950ca049b10d16f740c9ba08e07
Marcus Eisele <marcus.eisele@gmail.com>
2016-04-06 23:20:51 +0200

    change interval in server mock

--- app/controllers/handler.server.js
+++ app/controllers/handler.server.js
@@ -1,14 +1,11 @@
 'use strict';
 
 function handler () {
-   var count = 0;
-
+   
    this.getCount = function (req, res) {
 
-      count = count + 1;
-      console.log(count);
       res.contentType('application/json');
-      if(count%2 === 0){
+      if(Math.random() > 0.5){
          res.send( '{"notifications" : [{"id":"abc", "priority": 1, "number":' + Math.floor(Math.random()*100) +', "icon":"fa-phone-square" }]}' );
       }else{
          res.send( '{"notifications" : [{"id":"abc", "priority": 1, "number":' + Math.floor(Math.random()*100) +', "icon":"fa-phone-square" }, {"id":"def", "priority": 51, "number":' + Math.floor(Math.random()*100) +', "icon":"fa-envelope-square" }]}' );
